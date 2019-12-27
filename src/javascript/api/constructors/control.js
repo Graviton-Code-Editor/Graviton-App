9efb0becfcff84efa42ed23a0d0c7f1d2acaef5a
PJTSearch <pjtsignups@gmail.com>
2019-12-27 14:57:30 -0800

    add click event and rightAlign to Control

--- src/javascript/api/constructors/control.js
+++ src/javascript/api/constructors/control.js
@@ -17,12 +17,16 @@ new Control({
 */
 
 module.exports = {
-  Control: function ({ text, hint, screen = current_screen.id }) {
+  Control: function ({ text, hint, alignRight, screen = current_screen.id, click }) {
     this.text = text
     this.hint = hint
+    this.alignRight = alignRight
     this.screen = screen;
+    this.click = click;
     const controlSpan = document.createElement('span')
     controlSpan.innerText = text
+    if (click != undefined) controlSpan.addEventListener("click",click)
+    controlSpan.style.float = alignRight === true ? "right" : "left"
     if (hint != undefined) controlSpan.title = hint
     document
       .getElementById(this.screen)
