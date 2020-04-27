9f65715e65f5203b5e1a42ca23121401d8953c7a
Marc Espín Sanz <mespinsanz@gmail.com>
2020-04-27 09:49:25 +0200

    :bug: Fixed duplicated string in Store's title

--- src/javascript/defaults/windows/store.js
+++ src/javascript/defaults/windows/store.js
@@ -26,9 +26,9 @@ function Store(){
 		<div mounted="${mounted}" style="min-width:100%;">
 			<SideMenu default="home" style="min-height:100%;">
 				<div>
-					<H1 lang-string="Store">Store</H1>
+					<H1 lang-string="Store"></H1>
 					<label to="home">Home</label>
-					<label to="installed">Installed</label>
+					<label lang-string="Installed"></label>
 				</div>
 				<div>
 					<div class="home" href="home">
