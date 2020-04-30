489d760927d3402075aff66245cc76f5e7cbbd7b
Marc Espín Sanz <mespinsanz@gmail.com>
2020-04-30 20:33:49 +0200

    :sparkles: Fully translated to Spanish with the new format and strings

--- src/javascript/components/menu.js
+++ src/javascript/components/menu.js
@@ -64,80 +64,83 @@ function onMenuHovering(){
 		toggleMenuStatus(this)
 	}
 }
+
+const styleWrapper = style`
+	&{
+		display:block;
+		white-space:nowrap;
+		left:0;
+	}
+	& > button{
+		min-height:100%;
+		background:var(--menuButtonBackground);
+		color:var(--menuButtonText);
+		border:none;
+		outline:0;
+		font-size:13px;
+	}
+	& > button:hover{
+		min-height:100%;
+		background:var(--menuButtonHoveringBackground);
+		color:var(--menuButtonHoveringText);
+	}
+	& > button.active , & button:focus{
+		background:var(--menuButtonHoveringBackground);
+	}
+	& > div{
+		position:absolute;
+		background:var(--menuBackground);
+		padding:5px;
+		border-radius:5px;
+		border-top-left-radius:0px;
+		box-shadow:0px 2px 10px rgba(0,0,0,0.2);
+		z-index:1;
+	}
+	& > div p{
+		margin:0;
+		padding:0;
+		height:16px;
+	}
+	& > div svg{
+		right:15px;
+		width:8px;
+		position:absolute;
+		height:16px;
+	}
+	& > div a{
+		left:0;
+		display:flex;
+		font-size:13px;
+		padding:7px 8px;
+		min-width:100px;
+		width: auto;
+		border-radius:5px;
+		color:var(--menuOptionText);
+		cursor:pointer;
+		align-items:center;
+		padding-right:25px;
+	}
+	& > div a:hover{
+		background:var(--accentColor);
+		color:var(--menuOptionHoveringText);
+	}
+	& > div a:hover svg path{
+		fill:var(--menuOptionHoveringText);
+	}
+	& span{
+		height:2px;
+		border-radius:100px;
+		width:90%;
+		display:block;
+		background:var(--menuDivider);
+		margin:5px auto;
+		box-sizing:border-box;
+	}
+`
+
 function MenuComp(){
 	return element`
-		<div mounted="${mounted} :click="${onMenuClicked}" :mousemove="${onMenuHovering}" showed="false" class="${style`
-			&{
-				display:block;
-				white-space:nowrap;
-				left:0;
-			}
-			& > button{
-				min-height:100%;
-				background:var(--menuButtonBackground);
-				color:var(--menuButtonText);
-				border:none;
-				outline:0;
-				font-size:13px;
-			}
-			& > button:hover{
-				min-height:100%;
-				background:var(--menuButtonHoveringBackground);
-				color:var(--menuButtonHoveringText);
-			}
-			& > button.active , & button:focus{
-				background:var(--menuButtonHoveringBackground);
-			}
-			& > div{
-				position:absolute;
-				background:var(--menuBackground);
-				padding:5px;
-				border-radius:5px;
-				border-top-left-radius:0px;
-				box-shadow:0px 2px 10px rgba(0,0,0,0.2);
-				z-index:1;
-			}
-			& > div p{
-				margin:0;
-				padding:0;
-				height:16px;
-			}
-			& > div svg{
-				right:15px;
-				width:8px;
-				position:absolute;
-				height:16px;
-			}
-			& > div a{
-				left:0;
-				display:flex;
-				font-size:13px;
-				padding:7px 8px;
-				min-width:100px;
-				border-radius:5px;
-				color:var(--menuOptionText);
-				cursor:pointer;
-				align-items:center;
-				padding-right:20px;
-			}
-			& > div a:hover{
-				background:var(--accentColor);
-				color:var(--menuOptionHoveringText);
-			}
-			& > div a:hover svg path{
-				fill:var(--menuOptionHoveringText);
-			}
-			& span{
-				height:2px;
-				border-radius:100px;
-				width:90%;
-				display:block;
-				background:var(--menuDivider);
-				margin:5px auto;
-				box-sizing:border-box;
-			}
-		`} dropmenu">
-		</div>
+		<div mounted="${mounted} :click="${onMenuClicked}" :mousemove="${onMenuHovering}" showed="false" class="${styleWrapper} dropmenu"/>
 	`
 }
 
