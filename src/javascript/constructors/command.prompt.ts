37732d2b7fbf6736bdda98320f6b95039fcf0431
marc2332 <mespinsanz@gmail.com>
2020-07-28 23:55:13 +0200

    :building_construction: Passed Command Prompt component to TypeScript

---
+++ src/javascript/constructors/command.prompt.ts
@@ -0,0 +1,208 @@
+import { state, render, element } from '@mkenzo_8/puffin'
+import CommandPromptBody from '../components/command.prompt/command.prompt'
+import WindowBackground from '../components/window/background'
+
+import PuffinState from '../types/puffin.state'
+import { CommandPromptOptions } from '../types/command.prompt'
+
+class CommandPrompt {
+	private CPName: String
+	private CPCompletedEvent: (x) => void
+	private CPSelectedEvent: (x) => void
+	private CPScrolledEvent: (x) => void
+	private CPState: PuffinState
+	private CPOptions: any[]
+	private CPOptionsElement: HTMLElement
+	private CPInputValue: String
+
+	private _selectOption(option) {
+		if (option) {
+			const optionObj = findOptionAction(this.CPOptions, option)
+			if (optionObj.action) optionObj.action()
+			if (this.CPSelectedEvent)
+				this.CPSelectedEvent({
+					label: option.textContent,
+					data: option.props.data,
+				})
+		}
+		if (this.CPCompletedEvent) this.CPCompletedEvent(this.CPInputValue)
+	}
+
+	private _renderOptions(options) {
+		const self = this
+		let content = ''
+		let hoveredDefault = 0
+
+		const optionsComp = element`
+			<div>
+				${options.map(({ selected, label, data }, index) => {
+					if (selected) hoveredDefault = index
+					return element`<a :click="${onClicked}" data="${data}">${label}</a> `
+				})}
+			</div>
+		`
+		function onClicked() {
+			closeCommandPrompt(self.CPName)
+			self._selectOption(this)
+		}
+		this.CPOptionsElement.innerHTML = ''
+		render(optionsComp, this.CPOptionsElement)
+		this.CPState.data.hoveredOption = this.CPOptionsElement.children[0].children[hoveredDefault]
+		this._hoverOption(this.CPState.data.hoveredOption)
+	}
+
+	private _hoverOption(hoveredOption) {
+		if (!hoveredOption) return
+		const allOptions = [...hoveredOption.parentElement.children]
+		allOptions.forEach(option => {
+			if (option === hoveredOption) {
+				option.classList.add('active')
+				this.CPScrolledEvent({
+					label: option.textContent,
+					data: option.props.data,
+				})
+			} else {
+				option.classList.remove('active')
+			}
+		})
+	}
+
+	private _scrollOptions(scrollingDirection) {
+		const hoveredOption = this.CPState.data.hoveredOption
+		const allOptions = [...hoveredOption.parentElement.children]
+		const hoveredOptionPosition = allOptions.indexOf(hoveredOption)
+		if (scrollingDirection === 'up') {
+			if (hoveredOptionPosition !== 0) {
+				this.CPState.data.hoveredOption = allOptions[hoveredOptionPosition - 1]
+			} else {
+				this.CPState.data.hoveredOption = allOptions[allOptions.length - 1]
+			}
+		} else if (scrollingDirection === 'down') {
+			if (hoveredOptionPosition !== allOptions.length - 1) {
+				this.CPState.data.hoveredOption = allOptions[hoveredOptionPosition + 1]
+			} else {
+				this.CPState.data.hoveredOption = allOptions[0]
+			}
+		}
+		this._hoverOption(this.CPState.data.hoveredOption)
+	}
+
+	constructor({
+		name = Math.random(),
+		showInput = true,
+		inputPlaceHolder = '',
+		inputDefaultText = '',
+		options = [],
+		scrollOnTab = false,
+		closeOnKeyUp = false,
+		onCompleted = x => {},
+		onSelected = x => {},
+		onScrolled = x => {},
+	}: CommandPromptOptions) {
+		const finalName = `${name}_cp`
+		if (document.getElementById(finalName)) return // Check if there are any command prompts already opened
+
+		const self = this
+
+		this.CPName = finalName
+		this.CPCompletedEvent = onCompleted
+		this.CPSelectedEvent = onSelected
+		this.CPScrolledEvent = onScrolled
+		this.CPState = new state({
+			hoveredOption: null,
+		})
+		this.CPOptions = options
+		this.CPInputValue = ''
+
+		const configArguments = arguments[0]
+
+		const CommandPromptComponent = element({
+			components: {
+				CommandPromptBody,
+				WindowBackground,
+			},
+		})`
+		<CommandPromptBody mounted="${mounted}" id="${finalName}" :keydown="${scrolling}">
+			<WindowBackground window="${finalName}" closeWindow=${() => closeCommandPrompt(name)}/>
+			<div class="container">
+				<input value="${inputDefaultText}" style="${showInput ? '' : 'opacity:0; height:1px; margin:0;padding:0; border:0px;'}" placeHolder="${inputPlaceHolder}" :keyup="${writing}"/>
+				<div/>
+			</div>
+		</CommandPromptBody>
+		`
+		function writing(e) {
+			e.preventDefault()
+			const command = this.value
+			self.CPInputValue = command
+			switch (e.keyCode) {
+				case 9:
+					if (scrollOnTab) {
+						break
+					}
+					self._selectOption(self.CPState.data.hoveredOption)
+					closeCommandPrompt(finalName)
+				case 17:
+					if (!scrollOnTab) {
+						break
+					}
+				case 13:
+					self._selectOption(self.CPState.data.hoveredOption)
+					closeCommandPrompt(finalName)
+					break
+				case 27:
+				case 38:
+				case 40:
+					break
+				default:
+					self._renderOptions(filterOptions(this.value, options))
+			}
+		}
+		function scrolling(e) {
+			switch (e.keyCode) {
+				case 38:
+					self._scrollOptions('up')
+					break
+				case 9:
+					e.preventDefault()
+					if (!scrollOnTab) {
+						break
+					}
+				case 40:
+					self._scrollOptions('down')
+					break
+			}
+		}
+		function mounted() {
+			const container = this.children[1].children[1]
+			self.CPOptionsElement = container
+			self._renderOptions(self.CPOptions)
+			window.addEventListener('keydown', e => {
+				if (e.keyCode === 27) {
+					closeCommandPrompt(finalName)
+				}
+			})
+			const input = this.children[1].children[0]
+			input.selectionStart = input.selectionEnd = input.value.length
+			input.focus()
+		}
+		render(CommandPromptComponent, document.getElementById('windows'))
+	}
+}
+
+function closeCommandPrompt(CommandPromptComponent) {
+	if (document.getElementById(CommandPromptComponent)) document.getElementById(CommandPromptComponent).remove()
+}
+
+function filterOptions(search, options) {
+	return options
+		.map(function (option) {
+			if (option.label.match(new RegExp(search, 'i'))) return option
+		})
+		.filter(Boolean)
+}
+
+const findOptionAction = (options, option) => {
+	return options.find(opt => opt.label == option.textContent)
+}
+
+export default CommandPrompt
---
+++ src/javascript/types/command.prompt.ts
@@ -0,0 +1,14 @@
+interface CommandPromptOptions {
+	name?: String | number
+	showInput?: boolean
+	inputPlaceHolder?: String
+	inputDefaultText?: String
+	options: any[]
+	scrollOnTab?: boolean
+	closeOnKeyUp?: boolean
+	onCompleted?: (x) => void
+	onSelected?: (x) => void
+	onScrolled?: (x) => void
+}
+
+export { CommandPromptOptions }
