eaa9b66b7f67ad673ea0f7bb9b7e9e03a34c4833
marc2332 <mespinsanz@gmail.com>
2020-07-28 17:06:29 +0200

    :construction: Added Explorer constructor types

--- src/javascript/constructors/explorer.ts
+++ src/javascript/constructors/explorer.ts
@@ -1,8 +1,9 @@
 import { element } from '@mkenzo_8/puffin'
 import RunningConfig from 'RunningConfig'
 import Item from '../components/explorer/item'
+import { ExplorerOptions } from '../types/explorer'
 
-function Explorer({ items }) {
+function Explorer({ items }: ExplorerOptions) {
 	return element`
 		<div>
 			${items.map(item => {
--- src/javascript/types/explorer.ts
+++ src/javascript/types/explorer.ts
@@ -1,7 +1,22 @@
 import { StatusResult } from 'simple-git'
 
+interface ExplorerItemOptions {
+	label: String
+	items: this[]
+	mounted?: () => void
+	icon?: String
+	iconComp?: any
+	action?: () => void
+	contextAction?: () => void
+}
+
+interface ExplorerOptions {
+	items: ExplorerItemOptions[]
+}
+
+//Files Explorer Item
 interface ExplorerItem extends HTMLElement {
 	gitChanges: StatusResult
 }
 
-export { ExplorerItem }
+export { ExplorerItem, ExplorerOptions, ExplorerItemOptions }
