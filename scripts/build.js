f8123a24b8ee1cf5be2cc00f6631fd66eb5539e0
marc2332 <mespinsanz@gmail.com>
2020-08-11 16:08:28 +0200

    :bug: Fixed script:build

--- scripts/build.js
+++ scripts/build.js
@@ -1,23 +1,15 @@
 const fs = require('fs-extra')
 const path = require('path')
-const contributors = require('contributor-faces')
 
 const BUILD_DIR = path.join(__dirname, '..', 'assets', 'build.json')
 const buildData = {
 	date: new Date().toDateString(),
 }
 
-contributors().then(contributors => {
-	buildData.contributors = contributors.map(({ login }) => {
-		return {
-			name: login,
-		}
+fs.writeFile(BUILD_DIR, JSON.stringify(buildData))
+	.then(() => {
+		console.log(`✨ Created build's JSON.`)
 	})
-	fs.writeFile(BUILD_DIR, JSON.stringify(buildData))
-		.then(() => {
-			console.log(`✨ Created build's JSON.`)
-		})
-		.catch(err => {
-			console.log(err)
-		})
-})
+	.catch(err => {
+		console.log(err)
+	})
