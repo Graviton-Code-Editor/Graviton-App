894f2ac98ba9de786a46e4ffd0dc1bf9d73d88d5
marc2332 <mespinsanz@gmail.com>
2020-08-18 11:49:30 +0200

    :bug: Couldn't use LSP without the tsserver

--- plugins/dynamic/js-ts-langserver/graviton.config.js
+++ plugins/dynamic/js-ts-langserver/graviton.config.js
@@ -1,1 +1,31 @@
-module.exports.tasks = []
+const path = require('path')
+const { ncp } = require('ncp')
+const fs = require('fs-extra')
+
+module.exports.tasks = [
+	async function ({ distFolder }) {
+		await copy(path.resolve(__dirname, './node_modules/typescript/lib/'), path.join(distFolder, 'tslib'))
+	},
+]
+
+const copy = (from, to) => {
+	return new Promise(res => {
+		ncp(
+			from,
+			to,
+			{
+				filter(filePath) {
+					const fileName = path.basename(filePath)
+					const fileStats = fs.lstatSync(filePath)
+					if (fileStats.isDirectory() && fileName !== 'lib') return false
+					if (fileName === 'README.md' || fileName === 'watchGuard.js' || fileName === 'watchGuard.js') return false
+					if (fileName.match('typescript')) return false
+					return true
+				},
+			},
+			() => {
+				res()
+			},
+		)
+	})
+}
--- plugins/dynamic/js-ts-langserver/src/index.js
+++ plugins/dynamic/js-ts-langserver/src/index.js
@@ -1,8 +1,9 @@
 import path from 'path'
 
 export function entry({ RunningConfig }) {
+	console.log(__dirname, path.join(__dirname, 'ts', 'bin', 'tsserver'))
 	RunningConfig.emit('registerLanguageServer', {
 		modes: ['javascript', 'typescript'],
-		args: ['node', path.resolve(__dirname, 'cli.js'), '--stdio'],
+		args: ['node', path.resolve(__dirname, 'cli.js'), '--stdio', '--tsserver-path', path.join(__dirname, 'tslib', 'tsserver.js')],
 	})
 }
