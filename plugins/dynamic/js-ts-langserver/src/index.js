69478f524710c9a735851eb3642837927dcfcc15
marc2332 <mespinsanz@gmail.com>
2020-08-18 11:55:20 +0200

    :construction: Cleaned up

--- plugins/dynamic/js-ts-langserver/src/index.js
+++ plugins/dynamic/js-ts-langserver/src/index.js
@@ -1,7 +1,6 @@
 import path from 'path'
 
 export function entry({ RunningConfig }) {
-	console.log(__dirname, path.join(__dirname, 'ts', 'bin', 'tsserver'))
 	RunningConfig.emit('registerLanguageServer', {
 		modes: ['javascript', 'typescript'],
 		args: ['node', path.resolve(__dirname, 'cli.js'), '--stdio', '--tsserver-path', path.join(__dirname, 'tslib', 'tsserver.js')],
