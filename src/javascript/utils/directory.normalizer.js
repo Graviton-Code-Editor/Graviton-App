d87919280170e1893316e434b5dcfb129f5fcd91
Marc Espín Sanz <mespinsanz@gmail.com>
2020-09-08 22:17:26 +0200

    :bug: Bug fix

--- src/javascript/utils/directory.normalizer.js
+++ src/javascript/utils/directory.normalizer.js
@@ -6,7 +6,7 @@ const OS = eval('process.platform')
 function normalizeDir(dir, { isWSL = false } = {}) {
 	let dirNormalized = dir.split(/\\+/gm).join('\\')
 	dirNormalized = dirNormalized.replace(/"/g, '')
-	if (OS == 'win32') dirNormalized = dirNormalized.replace(/\/+/g, '\\').replace(/\\/g, '\\\\')
+	if (OS == 'win32') dirNormalized = dirNormalized.replace(/\/+/g, '\\')
 	if (isWSL) dirNormalized = dirNormalized.replace(/\+/g, '//')
 	return dirNormalized
 }
