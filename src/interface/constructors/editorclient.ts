db209f1e38878a7282467b0462c3e8820b8cd678
Marc <mespinsanz@gmail.com>
2020-11-14 23:42:06 +0100

    :memo: Added some notes

--- src/interface/constructors/editorclient.ts
+++ src/interface/constructors/editorclient.ts
@@ -1,9 +1,14 @@
 let editorClients = []
 
+/*
+ * The EditorClient provides an easy way to communicate
+ * from Graviton API to third-party libraries with an unified syntax
+ */
+
 class EditorClient {
 	public name: string
 	public type: string
-	public do: any
+	public do: (actionName: string, actionArgs: any) => any
 	constructor({ name = 'unknown', type }, object) {
 		this.name = name
 		this.type = type
--- src/interface/utils/test.ts
+++ src/interface/utils/test.ts
@@ -5,6 +5,11 @@ import Dialog from 'Constructors/dialog'
 import Notification from 'Constructors/notification'
 import * as puffin from '@mkenzo_8/puffin'
 
+/*
+ * This file is mainly for testing purposes
+ * It exposes globally some Core Modules
+ */
+
 if (!RunningConfig.data.isBrowser) {
 	if (eval('window.process.env.NODE_ENV') !== 'test') {
 		console.log(RunningConfig)
