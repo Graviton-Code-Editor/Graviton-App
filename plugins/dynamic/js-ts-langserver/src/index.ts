3a4c5ad674acec65905fb17f8ce3a339348589ca
Marc <mespinsanz@gmail.com>
2020-12-08 15:40:45 +0100

    :bug: Bug fix

--- plugins/dynamic/js-ts-langserver/src/index.ts
+++ plugins/dynamic/js-ts-langserver/src/index.ts
@@ -3,6 +3,6 @@ import path from 'path'
 export function entry({ RunningConfig }) {
 	RunningConfig.emit('registerLanguageServer', {
 		modes: ['javascript', 'typescript'],
-		args: ['node', path.resolve(__dirname, 'cli.js'), '--stdio', `--tsserver-path`,path.join(__dirname, 'tslib', 'tsserver.js')],
+		args: ['node', path.join(__dirname, 'cli.js'), '--stdio', `--tsserver-path`, path.join(__dirname, 'tslib', 'tsserver.js')],
 	})
 }
