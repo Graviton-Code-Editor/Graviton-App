df678c59d23a867514a802326d1e41b39600c2fa
Marc <mespinsanz@gmail.com>
2021-01-04 23:47:29 +0100

    :construction: Improveds shortcuts renaming dialog, you can now write 'empty' to leave it empty

--- src/interface/defaults/settings/pages/shortcuts.ts
+++ src/interface/defaults/settings/pages/shortcuts.ts
@@ -5,21 +5,30 @@ import InputDialog from '../../../utils/dialogs/dialog_input'
 import ShortcutsTable from '../../../components/settings/shortcuts_table'
 import { Button } from '@mkenzo_8/puffin-drac'
 
+enum States {
+	EMPTY = 'Empty',
+}
+
 function ShortcutCombo({ shortcut }) {
 	const { combos } = StaticConfig.data.appShortcuts[shortcut]
 
 	function changeCombo() {
 		InputDialog({
 			title: 'Change combo',
+			content: `Write 'empty' to leave it empty. `,
 		}).then(input => {
-			if (StaticConfig.data.appShortcuts[shortcut].combos[0]) {
+			if (input === 'empty') {
+				StaticConfig.data.appShortcuts[shortcut].combos.splice(0, 1)
+				this.innerText = States.EMPTY
+			} else if (StaticConfig.data.appShortcuts[shortcut].combos[0]) {
 				StaticConfig.data.appShortcuts[shortcut].combos[0] = input
+				this.innerText = input
 			} else {
 				StaticConfig.data.appShortcuts[shortcut].combos.push(input)
+				this.innerText = input
 			}
 
 			StaticConfig.triggerChange()
-			this.innerText = input
 
 			StaticConfig.emit('shortcutsHaveBeenUpdated')
 		})
@@ -30,7 +39,7 @@ function ShortcutCombo({ shortcut }) {
 			Button,
 		},
 	})`
-		<Button :click="${changeCombo}">${combos[0] || 'Empty'}</Button>
+		<Button :click="${changeCombo}">${combos[0] || States.EMPTY}</Button>
 	`
 }
 
