515d45e15e51c4f95755831ab25261dc51501110
Marc <mespinsanz@gmail.com>
2021-01-04 13:48:20 +0100

    :recycle: Cleaned up

--- webpack.config.js
+++
@@ -1,8 +0,0 @@
-const RendererConfig = require('./build/renderer_config')
-const BrowserConfig = require('./build/browser_config')
-const MainConfig = require('./build/main_config')
-const PreloadConfig = require('./build/preload_config')
-const TestConfig = require('./build/test_config')
-const ServerConfig = require('./build/server_config')
-
-module.exports = [RendererConfig, BrowserConfig, MainConfig, PreloadConfig, TestConfig, ServerConfig]
