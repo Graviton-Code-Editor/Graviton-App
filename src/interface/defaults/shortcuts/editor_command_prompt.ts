e35551f857f0fa2825bc2197e48ceef76a85cbc8
Marc <mespinsanz@gmail.com>
2021-01-12 21:59:36 +0100

    :bug: Couldn't have more than 1 file git diff opened at the same time

--- src/interface/defaults/shortcuts/editor_command_prompt.ts
+++ src/interface/defaults/shortcuts/editor_command_prompt.ts
@@ -76,6 +76,7 @@ RunningConfig.on('command.openEditorCommandPrompt', () => {
 						const fileExtension = getFormat(fileDir)
 
 						const { bodyElement, tabElement, tabState, isCancelled } = new Tab({
+							id: `${fileDir}_git_diff`,
 							title: `${basename}'s changes`,
 							isEditor: true,
 							explorerProvider: instance.explorerPovider,
