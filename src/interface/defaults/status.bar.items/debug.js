7560152278e47a9294231c7d301ad567fb4d2b3d
Marc <mespinsanz@gmail.com>
2021-01-30 13:31:03 +0100

    :bug: Fix: Debug Mode's status bar item wasn't being displayed

--- src/interface/defaults/status.bar.items/debug.js
+++ src/interface/defaults/status.bar.items/debug.js
@@ -2,13 +2,14 @@ import StatusBarItem from '../../constructors/status.bar.item'
 import RunningConfig from 'RunningConfig'
 import { element } from '@mkenzo_8/puffin'
 
-if (RunningConfig.data.isDebug) {
-	const DebugModeStatusBarItem = new StatusBarItem({
-		label: 'Debug',
-		position: 'left',
-		important: true,
-	})
-	RunningConfig.on('appLoaded', () => {
+RunningConfig.once('appLoaded', () => {
+	if (RunningConfig.data.isDebug) {
+		const DebugModeStatusBarItem = new StatusBarItem({
+			label: 'Debug',
+			position: 'left',
+			important: true,
+			hint: 'This window is running in debug mode.',
+		})
 		DebugModeStatusBarItem.show()
-	})
-}
+	}
+})
