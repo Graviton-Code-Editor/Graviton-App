7acb623e018c60ef35f6f98fd59e38475b397989
Marc Espín Sanz <mespinsanz@gmail.com>
2021-12-16 22:58:23 +0100

    :construction: Fixed tests

--- web/src/components/link.tsx
+++ web/src/components/link.tsx
@@ -1,5 +1,4 @@
-import { PropsWithChildren } from "react";
-import { shell } from "@tauri-apps/api";
+import { PropsWithChildren, useEffect, useState } from "react";
 import { isTauri } from "../utils/commands";
 import styled from "styled-components";
 
@@ -20,7 +19,19 @@ export default function Link(props: PropsWithChildren<any>) {
   if (isTauri) {
     const href = props.href;
     props = { ...props, href: null };
-    return <LinkStyled onClick={() => shell.open(href)} {...props} />;
+
+    const [openUrl, setOpenUrl] = useState<
+      (url: string) => Promise<void> | void
+    >((_) => {});
+
+    useEffect(() => {
+      // Dinamically load tauri's open.shell
+      import("@tauri-apps/api").then(({ shell }) => {
+        setOpenUrl(shell.open);
+      });
+    }, []);
+
+    return <LinkStyled onClick={() => openUrl(href)} {...props} />;
   }
   return <LinkStyled {...props} />;
 }
