14baa795d8b0cd4ff47406befe00e6915ee0cfd3
Marc Espin Sanz <mespinsanz@gmail.com>
2022-01-16 00:40:39 +0100

    :construction: Match more languages by it's file extension

--- core_api/src/filesystems/mod.rs
+++ core_api/src/filesystems/mod.rs
@@ -47,8 +47,23 @@ pub enum FileFormat {
 pub fn get_format_from_path(path: &str) -> FileFormat {
     if let Some(ext) = Path::new(path).extension() {
         match ext.to_str().unwrap() {
+            // HTML
+            "html" => FileFormat::Text("HTML".to_string()),
+            // CSS
+            "css" => FileFormat::Text("CSS".to_string()),
+            // Rust
             "rs" => FileFormat::Text("Rust".to_string()),
+            // Javascript
             "js" => FileFormat::Text("JavaScript".to_string()),
+            "jsx" => FileFormat::Text("JavaScript".to_string()),
+            // TypeScript
+            "ts" => FileFormat::Text("TypeScript".to_string()),
+            "tsx" => FileFormat::Text("TypeScript".to_string()),
+            // PHP
+            "php" => FileFormat::Text("PHP".to_string()),
+            // Python
+            "py" => FileFormat::Text("Python".to_string()),
+            // Not identified
             _ => FileFormat::Unknown,
         }
     } else {
