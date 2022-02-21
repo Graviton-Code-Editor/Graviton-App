1fcecff1f98df27fa0c566864f4e520cd2025522
Marc Espin Sanz <mespinsanz@gmail.com>
2022-02-21 14:25:33 +0100

    :memo: Added small todo comment

--- core_api/src/state_persistors/mod.rs
+++ core_api/src/state_persistors/mod.rs
@@ -4,6 +4,7 @@ pub mod file;
 pub mod memory;
 
 /// Persistor trait
+/// Make these methods async
 pub trait Persistor {
     fn load(&mut self) -> StateData;
     fn save(&mut self, data: &StateData);
