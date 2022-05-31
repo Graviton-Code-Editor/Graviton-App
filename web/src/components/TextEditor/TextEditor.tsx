5634b7da44fd9434f8575766384c8349dfab7266
marc2332 <mespinsanz@gmail.com>
2022-05-31 11:59:56 +0200

    :bug: Make all scrollbars visible again since it's a bit too buggy

--- web/src/components/TextEditor/TextEditor.tsx
+++ web/src/components/TextEditor/TextEditor.tsx
@@ -11,12 +11,6 @@ const CodeMirrorStyler = styled.div`
     background: ${({ theme }) => theme.elements.textEditor.background};
     box-shadow: inset 0px 5px 10px rgba(0, 0, 0, 0.1);
   }
-  & ::-webkit-scrollbar {
-    width: 10px !important;
-  }
-  &:not(:hover) ::-webkit-scrollbar-thumb {
-    background: transparent;
-  }
   & .ͼa {
     color: ${({ theme }) => theme.elements.textEditor.keyword.color};
   }
