1d90e93d29973217830352d1baec94215e7c3c46
marc2332 <mespinsanz@gmail.com>
2022-06-14 10:21:25 +0200

    :construction: Make i18 actually load indonesian

--- web/src/utils/translation.ts
+++ web/src/utils/translation.ts
@@ -2,10 +2,13 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 //@ts-ignore
 import EnglishLanguage from "@gveditor/languages/english";
+//@ts-ignore
+import IndonesianLanguage from "@gveditor/languages/indonesian";
 
 i18n.use(initReactI18next).init({
   resources: {
     [EnglishLanguage.code]: EnglishLanguage,
+    [IndonesianLanguage.code]: IndonesianLanguage,
   },
   lng: "en",
   fallbackLng: "en",
